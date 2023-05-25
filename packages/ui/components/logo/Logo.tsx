0d32203a776e702944fad8ac4e96eb7dc37e93da
Emerson <emerson@sygmasistemas.com.br>
2023-05-25 16:59:37 -0300

    att of projetct

--- packages/ui/components/logo/Logo.tsx
+++ packages/ui/components/logo/Logo.tsx
@@ -18,7 +18,7 @@ export default function Logo({
           <img className="mx-auto w-9 dark:invert" alt="Cal" title="Cal" src="/api/logo?icon=1" />
         ) : (
           <img
-            className={classNames(small ? "h-4 w-auto" : "h-5 w-auto", "")}
+            className={classNames(small ? "h-12 w-auto" : "h-5 w-auto", "")}
             alt="Cal"
             title="Cal"
             src="/logo.png"
